f0666853343696f79601a393464664a6c75659c4
RyanChapman-creator <ryanchapman2084@gmail.com>
2026-05-12 23:36:34 -0500

    Replaced file input stream with file reader

--- src/me/ryanchapman/flyaros/BotModelDAO.java
+++ src/me/ryanchapman/flyaros/BotModelDAO.java
@@ -1,10 +1,11 @@
 package me.ryanchapman.flyaros;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class BotModelDAO {
     
@@ -16,20 +17,19 @@ public class BotModelDAO {
     public final BotModel load(File file) {
         if (!file.getName().endsWith(".dat") && !file.getName().endsWith(".fly"))
             throw new RuntimeException("Flyaros data files should end in either .dat or .fly");
-        try (FileInputStream fis = new FileInputStream(file)) {
-            final byte[] bytes = fis.readNBytes(1024);
-            String data = new String(bytes)
-                .replace("\r", "");
-            if (!data.startsWith("flyaros.datafile v1.0.0\n")) {
+        try (FileReader fReader = new FileReader(file)) {
+            final List<String> data = fReader.readAllLines();
+            int line = 0;
+            if (!data.get(line).equals("flyaros.datafile v1.0.0")) {
                 final String errorMsg = String.format("The provided file '%s' cannot be loaded due to an invalid header.", file.getPath());
                 throw new RuntimeException(errorMsg);
             }
-            data = data.substring(data.indexOf('\n')+1);
-            if (!data.startsWith("bot.name=")) {
+            ++line;
+            if (!data.get(line).startsWith("bot.name=")) {
                 final String errorMsg = String.format("The provided file '%s' cannot be loaded due to the missing field 'bot.name'.", file.getPath());
                 throw new RuntimeException(errorMsg);
             }
-            String botName = Main.normalize(data.substring("bot.name=".length()));
+            String botName = Main.normalize(data.get(line).substring("bot.name=".length()));
             return new BotModel(botName);
         } catch (FileNotFoundException e) {
             final String errorMsg = String.format("The provided file '%s' could not be opened.", file.getPath());
