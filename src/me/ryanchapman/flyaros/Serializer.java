d4dd0d99f0fabe28b3e80758a0874d9e4a1d2c76
RyanChapman-creator <ryanchapman2084@gmail.com>
2026-05-26 16:55:56 -0500

    Allow flexible attribute appearance order in Flyaros data files

--- src/me/ryanchapman/flyaros/Serializer.java
+++ src/me/ryanchapman/flyaros/Serializer.java
@@ -3,7 +3,7 @@ package me.ryanchapman.flyaros;
 public class Serializer {
     
     final String serialize(final BotModel botModel) {
-        final StringBuilder sb = new StringBuilder("flyaros.datafile v1.1.0\n");
+        final StringBuilder sb = new StringBuilder("flyaros.datafile v1.2.0\n");
         sb.append("bot.name=").append(botModel.getName()).append('\n');
         sb.append("user.name=").append(botModel.getUserName());
         return sb.toString();
